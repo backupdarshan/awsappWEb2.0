ee08ba56fac6adcf1c10c63fb955d4dd5d9aee1e
backupdarshan <47488346+backupdarshan@users.noreply.github.com>
2019-02-13 03:05:32 +0530

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
+import { MainComponent } from './Dashboards/main/main.component';
 
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+];
 @NgModule({
-  exports: [ RouterModule ]
-
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ],
 })
-export class AppRoutingModule {
-
-  
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { CardComponent } from './components/card/card.component';
 import { MainComponent } from './Dashboards/main/main.component';
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule ,AppRoutingModule],
   declarations: [ AppComponent,
     CardComponent,
    MainComponent
